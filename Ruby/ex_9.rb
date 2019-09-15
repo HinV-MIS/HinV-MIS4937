ce669402358fc0cd612d51d246b5f0cbd1d525a5
HinV-MIS <hungvuongvan@yahoo.com>
2019-09-15 23:12:05 +0000

    ruby

---
+++ Ruby/ex_9.rb
@@ -0,0 +1,10 @@
+# Here's some new strange stuff, remember type it exactly.
+
+ days = "Mon Tue Wed Thu Fri Sat Sun"
+ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+ puts "Here are the days: ", days
+ puts "Here are the months: ", months
+
+ 
+    
