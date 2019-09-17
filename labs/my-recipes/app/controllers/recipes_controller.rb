459ba3279abea0b8ddb90049ba912962d63e3dd5
HinV-MIS <hungvuongvan@yahoo.com>
2019-09-17 19:51:09 +0000

    added labs with the restaurants app.

---
+++ labs/my-recipes/app/controllers/recipes_controller.rb
@@ -0,0 +1,10 @@
+class RecipesController< ApplicationController
+    def index
+        @recipes = Recipe.all
+        respond_to do |format|
+            format.html
+            format.json {render json: @recipes}
+        end
+        
+    end
+end
